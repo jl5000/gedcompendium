a7865b66e1dd72b4463e0da5e54d70d2e34d163d
Jamie Lendrum <jalendrum@gmail.com>
2021-02-03 22:29:09 +0000

    fix: add package documentation to get rid of weird warning

---
+++ R/gedcompendium-package.R
@@ -0,0 +1,10 @@
+#' @keywords internal
+"_PACKAGE"
+
+# The following block is used by usethis to automatically manage
+# roxygen namespace tags. Modify with care!
+## usethis namespace: start
+#' @import tidyged
+#' @import tidyged.io
+## usethis namespace: end
+NULL
