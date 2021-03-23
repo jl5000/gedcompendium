1d09f81d9ca106354fd390f3966f62b809dfb898
Jamie Lendrum <jalendrum@gmail.com>
2021-03-23 22:44:10 +0000

    fix: dependencies in namespace

--- R/gedcompendium-package.R
+++ R/gedcompendium-package.R
@@ -4,7 +4,6 @@
 # The following block is used by usethis to automatically manage
 # roxygen namespace tags. Modify with care!
 ## usethis namespace: start
-#' @import tidyged
-#' @import tidyged.io
+#' @import tidyged tidyged.io tidyged.utils visged
 ## usethis namespace: end
 NULL
